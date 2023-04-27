afbd17c38ed2a332671d9e93770a8198bf25c0f9
samleow <samuel.leow@gmail.com>
2023-04-27 15:09:55 +0800

    Sample physics scene

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
+import { Physics, usePlane, useBox } from '@react-three/cannon'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeGeometry args={[1000, 1000]} />
+      <shadowMaterial color="#171717" transparent opacity={0.4} />
+    </mesh>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Cube(props) {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+  return (
+    <mesh receiveShadow castShadow ref={ref}>
+      <boxGeometry />
+      <meshLambertMaterial color="hotpink" />
+    </mesh>
+  )
+}
+
+ReactDOM.render(
+  <Canvas shadows dpr={[1, 2]} gl={{ alpha: false }} camera={{ position: [-1, 5, 5], fov: 45 }}>
+    <color attach="background" args={['lightblue']} />
+    <ambientLight />
+    <directionalLight position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
+    <Physics>
+      <Plane position={[0, -2.5, 0]} />
+      <Cube position={[0.1, 5, 0]} />
+      <Cube position={[0, 10, -1]} />
+      <Cube position={[0, 20, -2]} />
+    </Physics>
+  </Canvas>,
+  document.getElementById('root')
+)
