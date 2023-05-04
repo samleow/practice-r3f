5fa2a8540af2b6164e67621e8bd2ebd76df50018
samleow <samuel.leow@gmail.com>
2023-05-04 13:02:07 +0800

    Split main webpage elements from canvas. Created temporary overlay page over background canvas.

---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+
+
+
+export function Homepage()
+{
+    return (
+        <div style={{position:'absolute', top: 0, left: 0, width: '100%', height: '100%'}}>
+            <h2>Homepage</h2>
+            <p>Lorem ipsum</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,14 @@
-// import ReactDOM from 'react-dom'
-import { Canvas } from '@react-three/fiber'
-import { Physics, usePlane, useBox } from '@react-three/cannon'
 import './index.css'
 import { createRoot } from 'react-dom/client';
+import { Scene } from './Scene';
+import { Homepage } from './Homepage';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-function Plane(props) {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
-  return (
-    <mesh ref={ref} receiveShadow>
-      <planeGeometry args={[1000, 1000]} />
-      <shadowMaterial color="#171717" transparent opacity={0.4} />
-    </mesh>
-  )
-}
-
-function Cube(props) {
-  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 0.2, 0.5], ...props }))
-  return (
-    <mesh receiveShadow castShadow ref={ref}>
-      <boxGeometry />
-      <meshLambertMaterial color="hotpink" />
-    </mesh>
-  )
-}
-
 root.render(
-  <Canvas shadows dpr={[1, 2]} gl={{ alpha: false }} camera={{ position: [-1, 5, 5], fov: 45 }}>
-    <color attach="background" args={['lightblue']} />
-    <ambientLight />
-    <directionalLight position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
-    <Physics>
-      <Plane position={[0, -2.5, 0]} />
-      <Cube position={[0.1, 5, 0]} />
-      <Cube position={[0, 10, -1]} />
-      <Cube position={[0, 20, -2]} />
-    </Physics>
-  </Canvas>
+  <>
+    <Scene />
+    <Homepage />
+  </>
 )
