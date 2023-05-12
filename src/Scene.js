7a662d787e156c1128fb5c5ba9a907be4858a868
samleow <samuel.leow@gmail.com>
2023-05-12 14:12:42 +0800

    Added shadows and highlight on hover. BUG: mouse events for the canvas are not reset after opening and closing page.

--- src/Scene.js
+++ src/Scene.js
@@ -39,32 +39,50 @@ function CreateGrid(setSelected) {
 
 function FloorPanel (coord) {
     
+    const [isFloorHovered, setFloorHover] = useState(false)
+    
     return (
-        <motion3d.mesh key={coord.index} position={[ coord.points[0]+0.5, -0.1, coord.points[1]+0.5]}
+        <motion3d.mesh receiveShadow key={coord.index} position={[ coord.points[0]+0.5, -0.1, coord.points[1]+0.5]}
         initial={{y: 7}}
         animate={{y: -0.1}}
         transition={{type: 'spring', duration: 0.5, delay: coord.index/20}}
         // TODO: make use of coord from onClick to set targetPos
         // maybe don't even need Select
+        //  onHoverStart & onHoverEnd not working
+        // onHoverStart={() => setFloorHover(true)}
+        // onHoverEnd={() => setFloorHover(false)}
+        //  onPointerOver checks if pointer is over the mesh
+        //  includes overlapping meshes !!
+        onPointerOver={() => setFloorHover(true)}
+        onPointerOut={() => setFloorHover(false)}
         onClick={(e) => console.log(e, coord)}
         >
             <boxGeometry args={[1, 0.2, 1]}/>
-            <meshLambertMaterial color="darksalmon" />
+            {/* motion3d for mesh materials will create errors on layout changes */}
+            {/* <motion3d.meshLambertMaterial color="darksalmon"
+            animate={{color: isFloorHovered?'rgb(255, 215, 0)':'rgb(233, 150, 122)'}}
+            transition={{duration: 0.4}}/> */}
+            <meshLambertMaterial color={isFloorHovered?'gold':'darksalmon'} />
         </motion3d.mesh>
     )
 }
 
 function Sphere(targetPos) {
     
+    const [isHovered, setHover] = useState(false)
+
     return (
-        <motion3d.mesh position={[ 0, 0.5, 0]}
+        <motion3d.mesh receiveShadow castShadow position={[ 0, 0.5, 0]}
         initial={{y: 7}}
         animate={{y: 0.5}}
-        whileHover={{x: 1}}
+        whileHover={{scale: 1.2}}
+        onPointerOver={() => setHover(true)}
+        onPointerOut={() => setHover(false)}
+        onClick={() => console.log('Clicked!')}
         transition={{y:{type: 'spring', duration: 0.7, delay: 3}}}
         >
             <sphereGeometry args={[0.5]}/>
-            <meshLambertMaterial color="lawngreen" />
+            <meshLambertMaterial color={isHovered?"crimson":"lawngreen"} />
         </motion3d.mesh>
     )
 }
@@ -84,7 +102,7 @@ export function Scene({ pageOn }) {
                 <color attach="background" args={['cornflowerblue']} />
                 <LayoutOrthographicCamera position={[5,3,5]} zoom={150}/>
                 <ambientLight />
-                <directionalLight position={[10, 6, 10]} castShadow shadow-mapSize={[2048, 2048]} />
+                <directionalLight position={[0, 6, 5]} castShadow shadow-mapSize={[2048, 2048]} />
                 <Sphere targetPos={targetPos}/>
 
                 <CreateGrid setSelected={setSelected}/>
